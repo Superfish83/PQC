09109f8d4414bacf80b37b2c39f274b9533d44a3
Kim Yeonjun <kyjun050803@gmail.com>
2024-12-20 00:24:18 +0900

    first commit

---
+++ message.cpp
@@ -0,0 +1,12 @@
+#define N   256
+#define p   3329
+
+class Polynomial {
+private:
+public:
+    
+
+    void init(int _n){
+        n = _n;
+    }
+};
---
+++ pqc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "BFVscheme.h"
+
+using namespace std;
+
+int main(){
+    printf("Hello cpp!\n");
+
+    return 0;
+}
